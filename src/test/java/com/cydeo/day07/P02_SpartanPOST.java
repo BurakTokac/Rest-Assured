81d99aa08e341008dcabdf03e0a2072d8b3ba406
mehmetCt <mehmet@cybertekschool.com>
2022-12-12 14:49:32 -0500

    Day07

--- src/test/java/com/cydeo/day07/P02_SpartanPOST.java
+++ src/test/java/com/cydeo/day07/P02_SpartanPOST.java
@@ -1,6 +1,8 @@
 package com.cydeo.day07;
 
 import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class P02_SpartanPOST extends SpartanTestBase {
 
     /**
-     Given accept type and Content type is JSON
+     Given accept type is JSON
+     And Content type is JSON
      And request json body is:
      {
      "gender":"Male",
@@ -28,5 +31,49 @@ public class P02_SpartanPOST extends SpartanTestBase {
     @DisplayName("POST spartan with String body")
     @Test
     public void test1() {
+
+        String requestBody="{\n" +
+                "     \"gender\":\"Male\",\n" +
+                "     \"name\":\"John Doe\",\n" +
+                "     \"phone\":8877445596\n" +
+                "     }";
+
+        String expectedMessage="A Spartan is Born!";
+
+        JsonPath jsonPath = given().accept(ContentType.JSON).log().body()// API send me response in JSON format
+                .contentType(ContentType.JSON) // API I am sending body in JSON format
+                .body(requestBody).  // This is the that we wnna POST in API
+                        when().post("/api/spartans").prettyPeek().
+                then().statusCode(201)
+                .contentType("application/json").extract().jsonPath();
+
+
+        assertEquals(expectedMessage,jsonPath.getString("success"));
+        assertEquals("John Doe",jsonPath.getString("data.name"));
+        assertEquals("Male",jsonPath.getString("data.gender"));
+        assertEquals(8877445596l,jsonPath.getLong("data.phone"));
+
+        // What if I want to get id
+        int id = jsonPath.getInt("data.id");
+        System.out.println("id = " + id);
+
+        // if you wanna do assertion with GET Request we can use it as Path param to specify same Spartans
+
+
+        /*
+        {
+            "success": "A Spartan is Born!",
+            "data": {
+                 "id": 174,
+                 "name": "John Doe",
+                 "gender": "Male",
+                 "phone": 8877445596l
+            }
+        }
+         */
+        //what happens if we run again? --> it will create new spartan with different ID
+
+
+
     }
 }
