ddb3c7640a59fe171310883cd0000eaf1e8f8fcf
mehmetCt <mehmet@cybertekschool.com>
2022-12-12 16:28:19 -0500

    Day07

--- src/test/java/com/cydeo/day07/P03_SpartanPUTPATCHDELETE.java
+++ src/test/java/com/cydeo/day07/P03_SpartanPUTPATCHDELETE.java
@@ -35,4 +35,27 @@ public class P03_SpartanPUTPATCHDELETE extends SpartanTestBase {
         // Create a GET method with same ID to see is it updated .
 
     }
+
+    @DisplayName("PATCH Spartan Single Spartan With Map ")
+    @Test
+    public void test2() {
+
+        // just like we did in POST we can use other options as well (String,POJO )
+        Map<String,Object> requestBody=new LinkedHashMap<>();
+        requestBody.put("name","James PATCH");
+
+
+        // PATCH to update existing record partially.So we need to existing record
+        int id=319;
+
+        given().log().body()// since we are not getting any response we dont need it accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .pathParam("id",id)
+                .body(requestBody).
+                when().patch("/api/spartans/{id}").
+                then().statusCode(204); // No Content --> It says I did successfuly but no response
+
+        // Create a GET method with same ID to see is it updated .
+
+    }
 }
