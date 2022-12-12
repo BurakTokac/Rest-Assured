fe55ca72a55e77a00abfd778dfa58cd3effc1cdd
mehmetCt <mehmet@cybertekschool.com>
2022-12-12 16:00:13 -0500

    Day07

---
+++ src/test/java/com/cydeo/day07/P03_SpartanPUTPATCHDELETE.java
@@ -0,0 +1,17 @@
+package com.cydeo.day07;
+
+import com.cydeo.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class P03_SpartanPUTPATCHDELETE extends SpartanTestBase {
+
+    @DisplayName("PUT Spartan Single Spartan With Map ")
+    @Test
+    public void test1() {
+
+
+    }
+}
