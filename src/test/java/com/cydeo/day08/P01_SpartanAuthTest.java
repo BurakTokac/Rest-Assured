72040327b9969a30ac2cca181954e4c8db025c91
mehmetCt <mehmet@cybertekschool.com>
2022-12-13 11:42:04 -0500

    Day08

--- src/test/java/com/cydeo/day08/P01_SpartanAuthTest.java
+++ src/test/java/com/cydeo/day08/P01_SpartanAuthTest.java
@@ -33,4 +33,61 @@ public class P01_SpartanAuthTest extends SpartanAuthTestBase {
 
     }
 
+
+    @DisplayName("DELETE /api/spartans/{id} as EDITOR  --> EXPECT --> 403 --> FORBIDDEN ")
+    @Test
+    public void test3() {
+
+        given().pathParam("id",56)
+                .accept(ContentType.JSON)
+                // .header("Accept","application/json") --> They created ready methods for us as Accept
+                .auth().basic("editor","editor").
+        when().delete("/api/spartans/{id}").then().statusCode(403)
+                .body("error",is("Forbidden"));
+
+    }
+    // Try to delete with Admin
+
+    @DisplayName("DELETE /api/spartans/{id} as ADMIN  --> EXPECT --> 204 --> SUCCESS ")
+    @Test
+    public void test4() {
+
+        given().pathParam("id",6)
+                .auth().basic("admin","admin").
+                when().delete("/api/spartans/{id}").then().statusCode(204);
+
+    }
+
+
+    /**
+     *  HOMEWORKS
+     *
+     *  	Role Based Control Test --> RBAC
+     *
+     * 			ADMIN  -->  GET  POST PUT PATCH  DELETE   --> Spartan Flow
+     * 			EDITOR -->  GET  POST PUT PATCH   403
+     * 			USER   -->  GET  403  403  403    403
+     * 			GUEST  -->  401  401  401  401    401
+     *
+     *
+     *   -- Create RBAC Test for all different roles from Spartan Application including with Negative Test cases
+     *   -- Create SpartanUtil Class
+     *             public static Map<String,Object>  SpartanUtil.getRandomSpartan(){
+     *                  Map<String,Object> spartanMap=new HashMap();
+     *
+     *                  use Faker class to create each time differetn spartan information
+     *
+     *                  return spartanMap;
+     *              }
+     *
+     *
+     *
+     */
+
+
+
+
+
+
+
 }
