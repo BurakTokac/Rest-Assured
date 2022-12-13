9b23baf2e577be71dd080a26aacd661a06ccb212
mehmetCt <mehmet@cybertekschool.com>
2022-12-13 14:38:46 -0500

    Day08

---
+++ src/test/java/com/cydeo/day08/P02_BookITTest.java
@@ -0,0 +1,15 @@
+package com.cydeo.day08;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+public class P02_BookITTest  {
+
+    @DisplayName("GET /api/campuses ")
+    @Test
+    public void test1() {
+
+
+    }
+}
