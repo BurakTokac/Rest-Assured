7f3b526f5f8c6807d17f4355c7375773e6101161
mehmetCt <mehmet@cybertekschool.com>
2022-12-13 11:11:15 -0500

    Day08

---
+++ src/test/java/com/cydeo/day08/P01_SpartanAuthTest.java
@@ -0,0 +1,16 @@
+package com.cydeo.day08;
+
+import com.cydeo.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+public class P01_SpartanAuthTest extends SpartanAuthTestBase {
+
+    @DisplayName("GET /spartans as GUEST user")
+    @Test
+    public void test1() {
+
+
+    }
+}
