1656f8f68456b673112fcaa41a799da6eb7bbb98
mehmetCt <mehmet@cybertekschool.com>
2022-12-13 14:43:39 -0500

    Day08

--- src/test/java/com/cydeo/day08/P02_BookITTest.java
+++ src/test/java/com/cydeo/day08/P02_BookITTest.java
@@ -1,14 +1,23 @@
 package com.cydeo.day08;
 
+import com.cydeo.utilities.BookitTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
-public class P02_BookITTest  {
+public class P02_BookITTest extends BookitTestBase {
+
+    String accessToken="Bearer eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMTMxMiIsImF1ZCI6InN0dWRlbnQtdGVhbS1sZWFkZXIifQ.ZIHJuDh19eLga3bLP7udnvNtEA0DM_W1H67ah2Zu3Lc";
 
     @DisplayName("GET /api/campuses ")
     @Test
     public void test1() {
+
+        given().accept(ContentType.JSON)
+                .header("Authorization",accessToken).
+        when().get("/api/campuses").prettyPeek()
+                .then().statusCode(200);
 
 
     }
