fba06cd49c5f45e6802e725840b36ff5a4c4ca16
mehmetCt <mehmet@cybertekschool.com>
2022-12-15 11:26:21 -0500

    Day08

--- src/test/java/com/cydeo/day08/P03_NewsAPI.java
+++ src/test/java/com/cydeo/day08/P03_NewsAPI.java
@@ -25,4 +25,29 @@ public class P03_NewsAPI extends NewsAPITestBase {
 
 
     }
+
+    /**
+     * TASK 2 —> X-Api-Key in HEADER
+     * - Given query param is q=“bitcoin”
+     * - And header is X-Api-Key=“yourKey”
+     * - When user sent request / everything endpoint - Then status code should be 200
+     * - And each articles contains “bitcoin”
+     */
+
+    @Test
+    public void test2() {
+
+        //d76cdd6edd704269a89f9742c91c75c4
+
+        given().log().uri().queryParam("q","bitcoin")
+                .header("X-Api-Key","d76cdd6edd704269a89f9742c91c75c4").
+                when().get("/everything").prettyPeek().then().statusCode(200);
+
+
+    }
+
+
+
+
+
 }
