3ae0ebc84b9e673d3fefb85a98652e54469eb3ad
mehmetCt <mehmet@cybertekschool.com>
2022-12-19 16:42:42 -0500

    Day10

--- src/test/java/com/cydeo/day09/P04_JsonSchemaValidation.java
+++ src/test/java/com/cydeo/day09/P04_JsonSchemaValidation.java
@@ -5,11 +5,14 @@ import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class P04_JsonSchemaValidation extends SpartanTestBase {
 
-    @DisplayName("GET /api/spartans/{id} to validate with JsonSchemaValidator")
+    @DisplayName("GET /api/spartans/{id} to validate with JsonSchemaValidator matchesJsonSchemaInClasspath")
     @Test
     public void test1() {
 
@@ -25,6 +28,19 @@ public class P04_JsonSchemaValidation extends SpartanTestBase {
 
           // if structure that we are getting matches with SingleSpartanSchema it will pass.
 
-
     }
+
+    @DisplayName("GET /api/spartans/{id} to validate with JsonSchemaValidator matchesJsonSchema")
+    @Test
+    public void test2() {
+
+        given().accept(ContentType.JSON)
+                .pathParam("id",45).
+                when().get("/api/spartans/{id}").prettyPeek()
+                .then().statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/resources/SingleSpartanSchema.json")));
+    }
+
+
+
 }
