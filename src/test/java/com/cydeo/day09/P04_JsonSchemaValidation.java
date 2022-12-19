6f22beaebd4f4c93c745587bd115b29fee30feb0
mehmetCt <mehmet@cybertekschool.com>
2022-12-19 16:33:48 -0500

    Day10

--- src/test/java/com/cydeo/day09/P04_JsonSchemaValidation.java
+++ src/test/java/com/cydeo/day09/P04_JsonSchemaValidation.java
@@ -1,6 +1,8 @@
 package com.cydeo.day09;
 
 import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
@@ -11,6 +13,17 @@ public class P04_JsonSchemaValidation extends SpartanTestBase {
     @Test
     public void test1() {
 
+        given().accept(ContentType.JSON)
+                .pathParam("id",45).
+        when().get("/api/spartans/{id}").prettyPeek()
+                .then().statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json"));
+
+          // it gets response from execution and we provided  structure of response in following path
+
+          //   --JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json")
+
+          // if structure that we are getting matches with SingleSpartanSchema it will pass.
 
 
     }
