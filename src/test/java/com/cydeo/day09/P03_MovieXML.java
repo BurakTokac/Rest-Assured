30b381cc80429ba0e7f4537d9a32acd067e87c7f
mehmetCt <mehmet@cybertekschool.com>
2022-12-19 15:46:52 -0500

    Day10

--- src/test/java/com/cydeo/day09/P03_MovieXML.java
+++ src/test/java/com/cydeo/day09/P03_MovieXML.java
@@ -31,8 +31,12 @@ public class P03_MovieXML{
 
         // get me year writer
 
-
-
-
     }
+
+    /**
+     *  HOMEWORK
+     * http://www.omdbapi.com/?apikey=81815fe6&s=Batman&page=2&r=xml
+     *
+     * - Try to ge all year information
+     */
 }
