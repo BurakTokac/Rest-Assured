5329ab054b37e8021c51cf54195500f0b633f0fc
mehmetCt <mehmet@cybertekschool.com>
2022-12-19 16:27:29 -0500

    Day10

---
+++ src/test/java/com/cydeo/day09/P04_JsonSchemaValidation.java
@@ -0,0 +1,17 @@
+package com.cydeo.day09;
+
+import com.cydeo.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class P04_JsonSchemaValidation extends SpartanTestBase {
+
+    @DisplayName("GET /api/spartans/{id} to validate with JsonSchemaValidator")
+    @Test
+    public void test1() {
+
+
+
+    }
+}
