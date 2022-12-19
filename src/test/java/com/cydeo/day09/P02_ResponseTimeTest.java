1c59171912daaadd930d7a103c5f45fe41049c65
mehmetCt <mehmet@cybertekschool.com>
2022-12-19 14:41:52 -0500

    Day10

---
+++ src/test/java/com/cydeo/day09/P02_ResponseTimeTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.day09;
+
+import com.cydeo.utilities.SpartanAuthTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class P02_ResponseTimeTest  extends SpartanAuthTestBase {
+
+    @DisplayName("GET /api/spartans to get response time ")
+    @Test
+    public void test1() {
+
+
+
+    }
+}
