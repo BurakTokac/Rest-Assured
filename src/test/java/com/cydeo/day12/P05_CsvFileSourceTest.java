db6f05219019f852adc0804366dc6ac3f4531006
mehmetCt <mehmet@cybertekschool.com>
2022-12-20 14:37:29 -0500

    Day12

--- src/test/java/com/cydeo/day12/P05_CsvFileSourceTest.java
+++ src/test/java/com/cydeo/day12/P05_CsvFileSourceTest.java
@@ -1,6 +1,5 @@
 package com.cydeo.day12;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -8,9 +7,11 @@ public class P05_CsvFileSourceTest {
 
 
     @ParameterizedTest
-    @CsvFileSource
-    public void test1() {
-
+    @CsvFileSource(resources = "/math.csv",numLinesToSkip =1 )
+    public void test1(int n1,int n2,int total) {
+        System.out.println("n1 = " + n1);
+        System.out.println("n2 = " + n2);
+        System.out.println("total = " + total);
 
     }
 }
