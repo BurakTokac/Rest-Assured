c1c993314f7bcbe3260f98ca0e6b8bade072c015
mehmetCt <mehmet@cybertekschool.com>
2022-12-20 10:29:56 -0500

    Day12

--- src/test/java/com/cydeo/day10/P01_Junit5AnnotationsLifeCycle.java
+++ src/test/java/com/cydeo/day10/P01_Junit5AnnotationsLifeCycle.java
@@ -7,22 +7,17 @@ public class P01_Junit5AnnotationsLifeCycle {
     // TestNG --> BeforeClass
     @BeforeAll
     public static void init(){
-
         System.out.println("----> BeforeAll is running");
-
     }
 
     @BeforeEach
     public void initEach(){
         System.out.println("--------> BeforeEach is running");
-
     }
 
     @Test
     public void test1() {
-
         System.out.println("----------> Test1 is running");
-
     }
 
     // Ignore Test --> TestNG -->@Ignore
@@ -30,15 +25,12 @@ public class P01_Junit5AnnotationsLifeCycle {
     @Disabled("This tc is ignored")
     @Test
     public void test2() {
-
         System.out.println("----------> Test2 is running");
-
     }
 
     @AfterEach
     public void afterEach(){
         System.out.println("--------> AfterEach is running");
-
     }
 
     //TestNG --> AfterClass
