6d11c9326a209cd2c14bf0250138e31e24213060
mehmetCt <mehmet@cybertekschool.com>
2022-12-20 12:49:34 -0500

    Day12

--- src/test/java/com/cydeo/day12/P04_CsvSource.java
+++ src/test/java/com/cydeo/day12/P04_CsvSource.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class P04_CsvSource {
 
     @ParameterizedTest
@@ -26,11 +29,37 @@ public class P04_CsvSource {
         "CO, Denver",
         "VA, Fairfax",
         "MA, Boston",
-        "NY, New York",
         "MD, Annapolis"
      */
     //verify place name contains your city name
     //print number of places for each request
+
+    @ParameterizedTest
+    @CsvSource({"NY, New York",
+            "CO, Denver",
+            "VA, Fairfax",
+            "MA, Boston",
+            "MD, Annapolis"})
+    public void testZipCodeDDT(String state,String city){
+
+        System.out.println("state = " + state);
+        System.out.println("city = " + city);
+        System.out.println("----------");
+
+
+        int placeNumber = given().baseUri("https://api.zippopotam.us")
+                .pathParam("state", state)
+                .pathParam("city", city).
+                when().get("us/{state}/{city}").
+                then().statusCode(200)
+                .body("places.'place name'", everyItem(containsString(city)))
+                .extract().jsonPath().getList("places").size();
+
+
+        System.out.println("------"+city+" has "+placeNumber+ " zipcode------");
+
+
+    }
 
 
 }
