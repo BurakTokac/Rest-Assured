63ee4053bbbe7cf2deff2445e4121f2a2910ec7d
mehmetCt <mehmet@cybertekschool.com>
2022-12-20 14:27:54 -0500

    Day12

---
+++ src/test/java/com/cydeo/day12/P05_CsvFileSourceTest.java
@@ -0,0 +1,16 @@
+package com.cydeo.day12;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class P05_CsvFileSourceTest {
+
+
+    @ParameterizedTest
+    @CsvFileSource
+    public void test1() {
+
+
+    }
+}
