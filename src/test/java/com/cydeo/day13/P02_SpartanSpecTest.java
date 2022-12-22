6dff2794e81886f57e49496af05b3ef2ccc5248a
mehmetCt <mehmet@cybertekschool.com>
2022-12-22 10:57:26 -0500

    Day13

--- src/test/java/com/cydeo/day13/P02_SpartanSpecTest.java
+++ src/test/java/com/cydeo/day13/P02_SpartanSpecTest.java
@@ -2,7 +2,12 @@ package com.cydeo.day13;
 
 import com.cydeo.utilities.SpartanNewTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.text.AbstractDocument;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class P02_SpartanSpecTest extends SpartanNewTestBase {
@@ -16,6 +21,25 @@ public class P02_SpartanSpecTest extends SpartanNewTestBase {
         then().
                 statusCode(200)
                 .contentType(ContentType.JSON);
+
+    }
+
+    @Test
+    public void getAllSpartansWithReqResSpec() {
+
+        RequestSpecification reqSpec = given().log().all().accept(ContentType.JSON)
+                .auth().basic("admin", "admin");
+
+
+        ResponseSpecification resSpec =
+                expect().statusCode(200)
+                .contentType(ContentType.JSON);
+
+
+        given().spec(reqSpec).
+        when().get("/spartans").
+        then().spec(resSpec);
+
 
     }
 }
