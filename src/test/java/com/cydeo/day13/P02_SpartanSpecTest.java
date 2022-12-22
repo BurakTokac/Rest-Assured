eee6ba0ceaa61ced7ec67dab694ee18a7aa8622f
mehmetCt <mehmet@cybertekschool.com>
2022-12-22 10:48:46 -0500

    Day13

---
+++ src/test/java/com/cydeo/day13/P02_SpartanSpecTest.java
@@ -0,0 +1,16 @@
+package com.cydeo.day13;
+
+import com.cydeo.utilities.SpartanNewTestBase;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class P02_SpartanSpecTest extends SpartanNewTestBase {
+
+    @Test
+    public void getAllSpartans() {
+
+
+
+
+    }
+}
