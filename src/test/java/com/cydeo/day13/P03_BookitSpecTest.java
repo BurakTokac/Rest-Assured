45d874be4be8f07bcde17978124c5f3e0e315874
mehmetCt <mehmet@cybertekschool.com>
2022-12-22 12:32:43 -0500

    Day13

--- src/test/java/com/cydeo/day13/P03_BookitSpecTest.java
+++ src/test/java/com/cydeo/day13/P03_BookitSpecTest.java
@@ -1,15 +1,28 @@
 package com.cydeo.day13;
 
+import com.cydeo.utilities.BookITUtils;
 import com.cydeo.utilities.BookitTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class P03_BookitSpecTest extends BookitTestBase {
+
+    /**
+     * Send a request /api/users/me endpoint as teacher
+     * verify status code 200
+     * content Type will be ContentType.JSON
+     *
+     */
+
     @Test
     public void test1() {
 
-
-
+            given().log().all()
+                    .header("Authorization", BookITUtils.getTokenByRole("teacher"))
+                    .accept(ContentType.JSON).
+            when().get("/api/users/me").prettyPeek().
+            then().statusCode(200).contentType(ContentType.JSON);
     }
 }
