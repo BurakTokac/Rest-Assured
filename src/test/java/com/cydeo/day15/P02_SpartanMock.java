ea6c94c349ad6fac495c0d90451409b4e3ba1a02
mehmetCt <mehmet@cybertekschool.com>
2022-12-27 16:16:24 -0500

    Day15

---
+++ src/test/java/com/cydeo/day15/P02_SpartanMock.java
@@ -0,0 +1,18 @@
+package com.cydeo.day15;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class P02_SpartanMock {
+
+    // https://4c678f45-0fdd-416e-b62d-bc4b94705f56.mock.pstmn.io
+
+    @DisplayName("GET /api/hello")
+    @Test
+    public void test() {
+
+
+
+    }
+}
