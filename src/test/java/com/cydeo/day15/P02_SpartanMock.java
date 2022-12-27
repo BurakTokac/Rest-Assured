a7d09bc590edde0d748af48cea0194ab832f66fc
mehmetCt <mehmet@cybertekschool.com>
2022-12-27 16:18:51 -0500

    Day15

--- src/test/java/com/cydeo/day15/P02_SpartanMock.java
+++ src/test/java/com/cydeo/day15/P02_SpartanMock.java
@@ -1,5 +1,7 @@
 package com.cydeo.day15;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
@@ -12,6 +14,10 @@ public class P02_SpartanMock {
     @Test
     public void test() {
 
+        Response response = given().log().all()
+                .accept(ContentType.TEXT).
+                when().get("/api/hello").prettyPeek()
+                .then().statusCode(200).extract().response();
 
 
     }
